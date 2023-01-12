262ed735156fbd03235450567860f74a4a57371c
Tomasz Kruczek <45160962+marendil@users.noreply.github.com>
2023-01-12 11:52:13 +0100

    POC

--- src/main/kotlin/com/example/DemoController.kt
+++ src/main/kotlin/com/example/DemoController.kt
@@ -2,13 +2,12 @@ package com.example
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
 
 @Controller("/demo")
 class DemoController {
 
-    @Get(uri="/", produces=["text/plain"])
-    fun index(): String {
-        return "Example Response"
-    }
+    @Get(uri="/", produces=["application/json"])
+    fun index() = ArrayList<AB>()
 }
+
+data class AB(val someField: String)
