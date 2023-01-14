561c6404d226cf0d5efcbb055f25021e0ed4ccd7
Daryl Robbins <daryl@robbins.name>
2023-01-14 10:00:13 -0500

    Reproduce issue with injection and values

--- src/main/kotlin/com/example/DemoController.kt
+++ src/main/kotlin/com/example/DemoController.kt
@@ -1,10 +1,16 @@
 package com.example
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import jakarta.inject.Inject
 
 @Controller("/demo")
-class DemoController {
+class DemoController(
+    // Fails with either of these two present:
+    @Inject val demoService: DemoService,
+    @Value("\${baseurl:}") val baseUrl: String,
+) {
 
     @Get(uri="/", produces=["application/json"])
     fun index() = ArrayList<AB>()
---
+++ src/main/kotlin/com/example/DemoService.kt
@@ -0,0 +1,9 @@
+package com.example
+
+import jakarta.inject.Singleton
+
+@Singleton
+class DemoService {
+
+    fun foo() = "bar"
+}
